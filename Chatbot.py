4c4fad51b3befa11a34b80220bdf02b507079db3
Institutes01 <109610291+Institutes01@users.noreply.github.com>
2023-12-13 19:03:17 +0000

    First CHanges post generation

--- Chatbot.py
+++ Chatbot.py
@@ -7,10 +7,10 @@ with st.sidebar:
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("💬 Chatbot")
-st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
+st.title("💬 TI Chatbot")
+st.caption("🚀 A streamlit front ended chatbot powered by OpenAI LLM API calls")
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "helpful HR assistant", "content": "How can I help you?"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
@@ -27,3 +27,5 @@ if prompt := st.chat_input():
     msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
+    st.markdown("*Streamlit* is **really** ***cool***.")
+    st.info(prompt)
